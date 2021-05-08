2fa86b667b38c49a11501f024c879d7d3660d4d9
BonzoDooDah <scottbukowski@hotmail.com>
2021-05-08 13:13:50 +0100

    Added fire control for lasers and refined PlayerControl script

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerControl : MonoBehaviour {
+    [Header("General Settings")]
+    [Tooltip("Speed at which player ship moves around the screen")]
     [SerializeField] private float controlSpeed = 35f;
+    [Tooltip("Maximum horizontal range (+/-)")]
     [SerializeField] private float xRange = 12f;
+    [Tooltip("Maximum vertcal range (+/-)")]
     [SerializeField] private float yRange = 7.5f;
 
+    [Header("Movement Settings")]
+    [Tooltip("Vertical adjustment of player ship depending on position on screen")]
     [SerializeField] private float pitchFactor = -2f;
+    [Tooltip("Vertical adjustment of player ship depending on movement")]
     [SerializeField] private float pitchMovement = -10f;
+    [Tooltip("Horizontal adjustment of player ship depending on position on screen")]
     [SerializeField] private float yawFactor = 2f;
+    [Tooltip("Horizontal adjustment of player ship depending on movement")]
     [SerializeField] private float rollMovement = -20f;
+
+    [Header("Weapon Prefabs")]
+    [Tooltip("Array of GameObjects for the player ship's laser weapons")]
+    [SerializeField] private GameObject[] lasers;
 
     float xThrow = 0f;
     float yThrow = 0f;
@@ -18,6 +31,7 @@ public class PlayerControl : MonoBehaviour {
     private void Update() {
         ProcessTranslation();
         ProcessRotation();
+        ProcessFiring();
     }
 
     private void ProcessTranslation() {
@@ -43,5 +57,20 @@ public class PlayerControl : MonoBehaviour {
         float roll = xThrow * rollMovement;
 
         transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    private void ProcessFiring() {
+        if (Input.GetButton("Fire1")) {
+            ActivateLasers(true);
+        } else {
+            ActivateLasers(false);
+        }
+    }
+
+    private void ActivateLasers(bool isActive) {
+        foreach (GameObject laserItem in lasers) {
+            var em = laserItem.GetComponent<ParticleSystem>().emission;
+            em.enabled = isActive;
+        }
     }
 }
