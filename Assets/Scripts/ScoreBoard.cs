f0f270d500a2da21c8c29d7e62643b847e6079c6
BonzoDooDah <scottbukowski@hotmail.com>
2021-05-08 17:38:31 +0100

    Set up score board UI

--- Assets/Scripts/ScoreBoard.cs
+++ Assets/Scripts/ScoreBoard.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreBoard : MonoBehaviour {
     private int _score;
+    private Text _textScore;
+
+    private void Start() {
+        _textScore = GetComponent<Text>();
+        _textScore.text = _score.ToString("0000000000");
+    }
 
     public void IncreaseScore(int scoreIncrease) {
         _score += scoreIncrease;
-        Debug.Log(_score);
+        _textScore.text = _score.ToString("0000000000");
     }
 }
