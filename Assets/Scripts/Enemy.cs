88c1a5cc25e7c7cbf2e3cc621ce043db1459a5f6
BonzoDooDah <scottbukowski@hotmail.com>
2021-05-10 20:57:30 +0100

    Redesigned enemy ships

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,12 +7,22 @@ public class Enemy : MonoBehaviour {
     [SerializeField] private int scorePerHit = 5;
     [SerializeField] private GameObject hitPrefab = null;
     [SerializeField] private GameObject explosionPrefab = null;
-    [SerializeField] private Transform parent = null;
+    private GameObject parentGameObject = null;
 
     private ScoreBoard _scoreBoard = null;
 
     private void Start() {
         _scoreBoard = FindObjectOfType<ScoreBoard>();
+        AddRigidbody();
+        parentGameObject = GameObject.FindWithTag("CreatedAtRuntime");
+    }
+
+    private void AddRigidbody() {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if (!rb) {
+            rb = gameObject.AddComponent<Rigidbody>();
+            rb.useGravity = false;
+        }
     }
 
     private void OnParticleCollision(GameObject other) {
@@ -23,7 +33,7 @@ public class Enemy : MonoBehaviour {
     private void ProcessHit() {
         if (hitPrefab) {
             GameObject obj = Instantiate(hitPrefab, transform.position, Quaternion.identity);
-            if (parent) obj.transform.parent = parent;
+            if (parentGameObject) obj.transform.parent = parentGameObject.transform;
         }
         numberOfHits--;
         _scoreBoard.IncreaseScore(scorePerHit);
@@ -32,7 +42,7 @@ public class Enemy : MonoBehaviour {
     private void ProcessDestruction() {
         if (explosionPrefab) {
             GameObject obj = Instantiate(explosionPrefab, transform.position, Quaternion.identity);
-            if (parent) obj.transform.parent = parent;
+            if (parentGameObject) obj.transform.parent = parentGameObject.transform;
         }
         Destroy(gameObject);
     }
